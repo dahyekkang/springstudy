7e2f568130593b6d4be177e44660d45a34e49a2a
kdh23232 <kdh23232@naver.com>
2023-09-25 17:39:16 +0900

    20230925

---
+++ 02_IoC_ex/src/main/java/com/gdu/app02/xml02/MyJdbcService.java
@@ -0,0 +1,30 @@
+package com.gdu.app02.xml02;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MyJdbcService {
+  
+  private MyJdbcDao myJdbcDao;    // service는 dao를 필요로 한다.
+  
+  public void add() {
+    myJdbcDao.add();
+  }
+  
+  public void remove() {
+    myJdbcDao.remove();
+  }
+  
+  public void modify() {
+    myJdbcDao.modify();
+  }
+  
+  public void select() {
+    myJdbcDao.select();
+  }
+  
+}
