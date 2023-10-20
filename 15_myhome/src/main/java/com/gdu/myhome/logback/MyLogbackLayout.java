9291b6edf03f7e9092dc4c096b41a4869271e071
kdh23232 <kdh23232@naver.com>
2023-10-20 17:45:30 +0900

    20231020

--- 15_myhome/src/main/java/com/gdu/myhome/logback/MyLogbackLayout.java
+++ 15_myhome/src/main/java/com/gdu/myhome/logback/MyLogbackLayout.java
@@ -8,25 +8,33 @@ import ch.qos.logback.core.LayoutBase;
 public class MyLogbackLayout extends LayoutBase<ILoggingEvent> {
 
   @Override
-  public String doLayout(ILoggingEvent event) { // event : 로그 관련 정보
+  public String doLayout(ILoggingEvent event) {   // event : 로그 관련 정보
     
     StringBuilder sb = new StringBuilder();
+    
     sb.append("[");
     sb.append(new SimpleDateFormat("HH:mm:ss").format(event.getTimeStamp()));
     sb.append("]");
-    sb.append(String.format("%-5s", event.getLevel()));
-    sb.append(":");
+    
     String loggerName = event.getLoggerName();
-    sb.append(loggerName);
+
+    if(!loggerName.endsWith("RequestCheckAop")) {      
+      sb.append(String.format("%-5s", event.getLevel()));
+      sb.append(":");
+      sb.append(loggerName);
+    }
+    
     if(loggerName.equals("jdbc.sqlonly")) {
       sb.append("\n    ");
     } else {
       sb.append(" - ");
     }
+    
     sb.append(event.getFormattedMessage());
     sb.append("\n");
     
     return sb.toString();
+    
   }
   
 }
